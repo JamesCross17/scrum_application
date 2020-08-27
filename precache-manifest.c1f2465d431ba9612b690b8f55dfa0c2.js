de7d5cb93b2c9abc3bba7e245f38962a51ad8bb4
JamesCross17 (via Travis CI) <stas_razuvanov@mail.ru>
2020-08-27 08:52:23 +0000

    Deploy JamesCross17/scrum_application to github.com/JamesCross17/scrum_application.git:gh-pages

---
+++ precache-manifest.c1f2465d431ba9612b690b8f55dfa0c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6939ccf53f67a37892800d3b1164a0ae",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c7b870ba26dd38ee8a50",
+    "url": "./static/css/2.04ff9426.chunk.css"
+  },
+  {
+    "revision": "116ca1477096e5adce68",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c7b870ba26dd38ee8a50",
+    "url": "./static/js/2.a6484e5a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.a6484e5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "116ca1477096e5adce68",
+    "url": "./static/js/main.1f89975f.chunk.js"
+  },
+  {
+    "revision": "dbab036f87ca1350243a",
+    "url": "./static/js/runtime-main.851de3a6.js"
+  }
+]);
